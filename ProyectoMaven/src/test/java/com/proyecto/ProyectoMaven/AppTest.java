15c3f248ae91e92b90d3bdec6f6f780deb6101f7
milla <milla@DESKTOP-JU94275>
2022-01-16 18:43:53 +0100

    Test

--- ProyectoMaven/src/test/java/com/proyecto/ProyectoMaven/AppTest.java
+++ ProyectoMaven/src/test/java/com/proyecto/ProyectoMaven/AppTest.java
@@ -1,20 +1,52 @@
 package com.proyecto.ProyectoMaven;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue(){
         assertTrue( true );
+    }
+    
+    @Test
+    public void testSuma() {
+        System.out.println("suma");
+        int num1 = 3;
+        int num2 = 5;
+        int expResult = 8;
+        int result = App.suma(num1, num2);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testResta() {
+        System.out.println("resta");
+        int num1 = 5;
+        int num2 = 7;
+        int expResult = 0;
+        int result = App.resta(num1, num2);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testMultiplicacion() {
+        System.out.println("multiplicacion");
+        int num1 = 10;
+        int num2 = 5;
+        int expResult = 50;
+        int result = App.multiplicacion(num1, num2);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testDivision() {
+        System.out.println("division");
+        int num1 = 10;
+        int num2 = 0;
+        int expResult = 0;
+        int result = App.division(num1, num2);
+        assertEquals(expResult, result);
     }
 }
